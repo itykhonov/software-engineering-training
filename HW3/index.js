06d3e8d83178b6cbe498c2c6e2d2d820e49d3217
igor tykhonov <tykhigo@hilti.com>
2019-04-16 11:35:50 +0300

    union and unionAll functionality

---
+++ HW3/index.js
@@ -0,0 +1,46 @@
+// require csvtojson
+var csv = require("csvtojson");
+
+// Convert a csv file with csvtojson
+var csv1 = csv().fromFile('./Barcelona1.csv');
+var csv2 = csv().fromFile('./barcelona2.csv');
+
+//when parse finished, result will be emitted here.
+Promise.all([csv1, csv2]).then(([arr1, arr2]) => {
+    console.time();
+    const unionArr = getOnlyNeededFields(union(arr1, arr2));
+    console.timeEnd();
+    //default: 971.482ms
+
+    console.log('unionArr', unionArr.length) //unionAllArr 8520
+
+    console.time();
+    const unionAllArr = getOnlyNeededFields(unionAll(arr1, arr2));
+    console.timeEnd();
+    //default: 17.985ms
+
+    console.log('unionAllArr', unionAllArr.length) //unionAllArr 9929
+});
+
+const union = (arr1, arr2) => [
+    ...arr1,
+    ...arr2.filter(el => !arr1.find(arr1El => el.listing_url.includes(arr1El.id)))
+        .map(getIdFromUrl),
+];
+
+const unionAll = (arr1, arr2) => [
+    ...arr1,
+    ...arr2.map(getIdFromUrl),
+];
+
+const getIdFromUrl = (obj) => {
+    const urlArr = obj.listing_url.split('/');
+    const id = urlArr[urlArr.length - 1];
+    const newData = Object.assign({}, obj, {id});
+    delete newData.listing_url;
+    return newData;
+}
+
+const getOnlyNeededFields = (arr) => {
+    return arr.map(({name, latitude, longitude}) => ({name, latitude, longitude}));
+}
